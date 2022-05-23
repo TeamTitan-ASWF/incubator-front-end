db5242533bc49207b68d35b0d9d558e0b72e5936
jpeastman <j2222307@MacBook-Pro-3.local>
2022-05-23 13:38:00 -0500

    3_S_Story3_001: table displayed, prior to deleting unused boilplate table code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Submit from "./components/Submit";
+import ReviewerList from "./components/Reviewer/ReviewerList";
 
 export default function App() {
-  return (
-    <div className="App">
-        <Submit/>
-    </div>
-  );
+    return (
+        <div className="App">
+            {/*<Submit/>*/}
+            <ReviewerList />
+        </div>
+    );
 }
