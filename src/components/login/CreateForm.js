bfb5ad56b14b45ada9869060be31d86078d46bf1
jpeastman <j2222307@MacBook-Pro-3.local>
2022-06-01 15:39:01 -0500

    user profile creation page complete

--- src/components/login/CreateForm.js
+++ src/components/login/CreateForm.js
@@ -1,4 +1,4 @@
-import {TextField} from "@mui/material";
+import {Select, TextField} from "@mui/material";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import Container from "@mui/material/Container";
@@ -9,16 +9,22 @@ import Grid from "@mui/material/Grid";
 import Rank from "../user/Rank";
 import DOB from "../user/DOB";
 import * as React from "react";
+import {AdapterDateFns} from "@mui/x-date-pickers/AdapterDateFns";
+import {DesktopDatePicker} from "@mui/x-date-pickers/DesktopDatePicker";
+import {LocalizationProvider} from "@mui/x-date-pickers/LocalizationProvider";
+import MenuItem from "@mui/material/MenuItem";
+import {formatDate} from "../inputValidation/dateValidationFunctions";
 
 export default function CreateForm({setCurrentPage, setUserCreated}) {
     const [errorMessage, setErrorMessage] = useState("");
+    const [dob, setDob] = useState(null);
 
     const submitForm = (e) => {
         setErrorMessage("");
         e.preventDefault();
 
-        console.log(e);
-        return;
+        //console.log(e);
+        //return;
 
         if (e.target.password.value !== e.target.confirmPassword.value) {
             setErrorMessage("Passwords do not match")
@@ -27,14 +33,20 @@ export default function CreateForm({setCurrentPage, setUserCreated}) {
 
         apiCall("createUser", 'add', {
             userName: e.target.userName.value,
-            password: e.target.password.value
+            password: e.target.password.value,
+            fName: e.target.fName.value,
+            lName: e.target.lName.value,
+            mI: e.target.mI.value,
+            dodId: e.target.dodId.value,
+            rank: e.target[10].value,
+            dob: formatDate(dob),
         }).then(handleResults);
     }
 
     const handleResults = (r) => {
-
         if (r.wasError) {
-            setErrorMessage(r.apiErrorMsg.response.data)
+            setErrorMessage("Something went wrong.");
+            console.log(r.apiErrorMsg.response.data)
             return;
         }
 
@@ -43,7 +55,6 @@ export default function CreateForm({setCurrentPage, setUserCreated}) {
     }
 
     return (
-
         <Container component="main" maxWidth="md" sx={{mb: 4}}>
             <Paper
                 variant="outlined"
@@ -58,41 +69,38 @@ export default function CreateForm({setCurrentPage, setUserCreated}) {
                         sx={{textAlign: 'center', mb: 1}}
                     >
                         Create your Incubator Account
-                    </Typography>
-                    <br/>
+                    </Typography><br/>
 
                     <Grid container spacing={3}>
                         <Grid item xs={12} sm={4}>
-                        <TextField
-                            required
-                            variant="outlined"
-                            label="User Name"
-                            id="userName"
-                        />
+                            <TextField
+                                required
+                                variant="outlined"
+                                label="User Name"
+                                id="userName"
+                            />
                         </Grid>
 
                         <Grid item xs={12} sm={4}>
-                        <TextField
-                            required
-                            id="password"
-                            error={errorMessage === "Passwords do not match"}
-                            variant="outlined"
-                            label="Password"
-                            type={"password"}
-
-                        />
-                            </Grid>
+                            <TextField
+                                required
+                                id="password"
+                                error={errorMessage === "Passwords do not match"}
+                                variant="outlined"
+                                label="Password"
+                                type={"password"}
+                            />
+                        </Grid>
 
                         <Grid item xs={12} sm={4}>
-                        <TextField
-                            required
-                            variant="outlined"
-                            error={errorMessage === "Passwords do not match"}
-                            id="confirmPassword"
-                            label="Confirm"
-                            type={"password"}
-
-                        />
+                            <TextField
+                                required
+                                variant="outlined"
+                                error={errorMessage === "Passwords do not match"}
+                                id="confirmPassword"
+                                label="Confirm"
+                                type={"password"}
+                            />
                         </Grid>
 
                         <Grid item xs={12} sm={5}>
@@ -141,19 +149,73 @@ export default function CreateForm({setCurrentPage, setUserCreated}) {
                         </Grid>
 
                         <Grid item xs={12} sm={2}>
-                            <Rank updateState={() => {}} propsID={"rank"} value={"E1"}/>
+                            <Select
+                                labelId="demo-simple-select-label"
+                                id="rank"
+                                name="rank"
+                                label="Rank"
+                                defaultValue="E1"
+                            >
+                                <MenuItem value={"E1"}>E1</MenuItem>
+                                <MenuItem value={"E2"}>E2</MenuItem>
+                                <MenuItem value={"E3"}>E3</MenuItem>
+                                <MenuItem value={"E4"}>E4</MenuItem>
+                                <MenuItem value={"E5"}>E5</MenuItem>
+                                <MenuItem value={"E6"}>E6</MenuItem>
+                                <MenuItem value={"E7"}>E7</MenuItem>
+                                <MenuItem value={"E8"}>E8</MenuItem>
+                                <MenuItem value={"E9"}>E9</MenuItem>
+                                <MenuItem value={"O1"}>O1</MenuItem>
+                                <MenuItem value={"O2"}>O2</MenuItem>
+                                <MenuItem value={"O3"}>O3</MenuItem>
+                                <MenuItem value={"O4"}>O4</MenuItem>
+                                <MenuItem value={"O5"}>O5</MenuItem>
+                                <MenuItem value={"O6"}>O6</MenuItem>
+                                <MenuItem value={"O7"}>O7</MenuItem>
+                                <MenuItem value={"O8"}>O8</MenuItem>
+                                <MenuItem value={"O9"}>O9</MenuItem>
+                                <MenuItem value={"O10"}>O10</MenuItem>
+                                <MenuItem value={"WO1"}>WO1</MenuItem>
+                                <MenuItem value={"CW2"}>CW2</MenuItem>
+                                <MenuItem value={"CW3"}>CW3</MenuItem>
+                                <MenuItem value={"CW4"}>CW4</MenuItem>
+                                <MenuItem value={"CW5"}>CW5</MenuItem>
+                            </Select>
+                            {/*<Rank updateState={() => {}} propsID={"rank"} value={"E1"}/>*/}
                         </Grid>
 
                         <Grid item xs={12} sm={4}>
-                            <DOB applicationInfo={{dob: "Tue Jan 01 1995 18:00:00 GMT-0600 (Central Standard Time)"}}
-                                 setApplicationInfo={() => {}}/>
+                            <LocalizationProvider dateAdapter={AdapterDateFns}>
+                                <DesktopDatePicker
+                                    label="Date of birth"
+                                    mask={"____-__-__"}
+                                    id="dob"
+                                    name="dob"
+                                    inputFormat="yyyy-MM-dd"
+                                    views={["year", "month", "day"]}
+                                    value={dob}
+                                    onChange={(value) => setDob(value)}
+                                    renderInput={(params) => <TextField {...params} />}
+                                />
+                            </LocalizationProvider>
+                            {/*<DOB applicationInfo={{dob: "Tue Jan 01 1995 18:00:00 GMT-0600 (Central Standard Time)"}}*/}
+                            {/*     setApplicationInfo={() => {}}/>*/}
                         </Grid>
                     </Grid>
 
                     <Typography color='error'>{errorMessage}</Typography>
+
                     <Button
                         variant="contained"
-                        sx={{mt: 1}}
+                        sx={{mr: 2, mt: 2}}
+                        onClick={() => setCurrentPage("login")}
+                    >
+                        Login
+                    </Button>
+
+                    <Button
+                        variant="contained"
+                        sx={{mt: 2}}
                         type={"submit"}
                     >
                         Create
@@ -161,6 +223,5 @@ export default function CreateForm({setCurrentPage, setUserCreated}) {
                 </form>
             </Paper>
         </Container>
-
     )
 }
--- src/components/login/LoginPage.js
+++ src/components/login/LoginPage.js
@@ -2,80 +2,73 @@ import {TextField} from "@mui/material";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import Paper from "@mui/material/Paper";
-import { useContext, useState } from "react";
+import {useContext, useState} from "react";
 import apiCall from "../api/api";
 import AppContext from "../contexts/AppContext";
 
 
+export default function LoginPage({setCurrentPage, userCreated, changePage}) {
+    const appContext = useContext(AppContext);
+    const [errorMessage, setErrorMessage] = useState("");
 
+    const login = (e) => {
+        e.preventDefault();
 
-export default function LoginPage({setCurrentPage, userCreated, changePage}){
-const appContext = useContext(AppContext);
-const [errorMessage,setErrorMessage] = useState("");
+        apiCall('login', 'add', {userName: e.target.userName.value, password: e.target.password.value}).then(setUser);
 
-const login = (e) => {
-    e.preventDefault();
-
-    apiCall('login','add',{userName : e.target.userName.value, password : e.target.password.value}).then(setUser);
-
-}
-
-
-const setUser = (r) => {
-    if(r.wasError){
-        setErrorMessage("Wrong Username or Password")
     }
-    appContext.setIsValidated(true)
-    appContext.setUser(r.apiData)
-    changePage("/");
-
-}
 
 
+    const setUser = (r) => {
+        if (r.wasError) {
+            setErrorMessage("Wrong Username or Password")
+        }
+        appContext.setIsValidated(true)
+        appContext.setUser(r.apiData)
+        changePage("/");
+
+    }
 
 
-return (
+    return (
+        <Paper
+            variant="outlined"
+            sx={{
+                my: {xs: 3, md: 6}, p: {xs: 2, md: 3},
+                textAlign: "center",
+                boxShadow: 20
+            }}>
+            {userCreated ? <> <Typography>Account Created Succesfully!</Typography> <br/> </> : ""}
 
-    
-
-    <Paper
-        variant="outlined"
-        sx={{
-            my: {xs: 3, md: 6}, p: {xs: 2, md: 3},
-            textAlign: "center",
-            boxShadow: 20
-        }}>
-                        {userCreated ?<> <Typography>Account Created Succesfully!</Typography>  <br/> </>: ""}
-                    
             <Typography>Login Page</Typography>
 
             <br/>
-            <form onSubmit={login}>  
-        <TextField
+            <form onSubmit={login}>
+                <TextField
                     required
-                    error = {errorMessage ? true : false}
+                    error={errorMessage ? true : false}
                     variant="outlined"
                     label="User Name"
                     id="userName"
-            
+
                 /><br/><br/>
-                    <TextField
+                <TextField
                     required
-                    error = {errorMessage ? true : false}
+                    error={errorMessage ? true : false}
                     id="password"
                     variant="outlined"
                     label="Password"
                     type={"password"}
-            
+
                 /><br/><br/>
-                <Typography color = 'error'>{errorMessage}</Typography>
+                <Typography color='error'>{errorMessage}</Typography>
                 <Button type="submit">Login</Button>
-                <Button onClick= {() => setCurrentPage("createForm")}>Create User</Button>
-                </form>
-    </Paper>
+                <Button onClick={() => setCurrentPage("createForm")}>Create User</Button>
+            </form>
+        </Paper>
 
 
-)
+    )
 
 
 }
