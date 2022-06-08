24d0c548ed9c547b603336883055d9f83e97a7c8
danielgebhardt <daniel.h.gebhardt.mil@swf.army.mil>
2022-06-08 14:46:18 -0500

    removed unused code in personalinfoform

--- src/components/application/PersonalInfoForm.js
+++ src/components/application/PersonalInfoForm.js
@@ -2,21 +2,11 @@ import * as React from 'react';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
-// import Rank from "./Rank";
-// import DOB from "./DOB";
 import LastACFT from "./LastACFT";
-import {fixTimeZone} from "../inputValidation/dateValidationFunctions";
-import {useContext, useEffect} from "react";
+import {formatDate} from "../inputValidation/dateValidationFunctions";
+import {useContext} from "react";
 import AppContext from "../contexts/AppContext";
-import Rank from "./Rank";
-import {LocalizationProvider} from "@mui/x-date-pickers/LocalizationProvider";
-import {AdapterDateFns} from "@mui/x-date-pickers/AdapterDateFns";
-import {DesktopDatePicker} from "@mui/x-date-pickers/DesktopDatePicker";
-import {useState} from "react";
-import {Popover, Popper} from "@mui/material";
-import Box from "@mui/material/Box";
 import Tooltip from "@mui/material/Tooltip";
-
 
 export default function PersonalInfoForm({
                                              applicationInfo,
@@ -27,23 +17,10 @@ export default function PersonalInfoForm({
                                          }) {
 
     const appContext = useContext(AppContext);
-    //const IS_NUMERIC_REGEX = /^[0-9]+$/;
+
     const IS_FLOAT_REGEX = /^[0-9]+[.]?[0-9]*$/;
     const IS_THREE_DIGITS_OR_LESS = /^[0-9]{0,3}$/;
     const IS_TWO_DIGITS_OR_LESS = /^[0-9]{0,2}$/;
-    const [anchorEl, setAnchorEl] = useState(null);
-    const [open,setOpen] = useState(false)
-
-
-
-    // fName: appContext.user?.fName ?? "",
-    // lName: appContext.user?.lName ?? "",
-    // mI: appContext.user?.mI ?? "",
-    // dodId: appContext.user?.dodId ?? "",
-    // rank: appContext.user?.rank ?? "E1",
-    // dob: (appContext.user?.dob) ? fixTimeZone(appContext.user?.dob) : 'Tue Jan 01 1995 18:00:00 GMT-0600 (Central Standard Time)',
-
-
 
     return (
         <React.Fragment>
@@ -55,116 +32,91 @@ export default function PersonalInfoForm({
 
 
                 <Tooltip title={'You must update your profile to edit this field'}>
-                <Grid item xs={12} sm={5}>
+                    <Grid item xs={12} sm={5}>
 
-                    <TextField
-                        required
-                        error = {errorList.includes("fName")}
-                        disabled={true}
-                        id="fName"
-                        name="firstName"
-                        label="First name"
-                        value={appContext.user?.fName || " "}
-                        fullWidth
-                        // autoComplete="given-name"
-                        variant="standard"
-                        // onChange={(e) => {
-                        //     updateState(e)
-                        //     onChangeValidate(e);
-                        // }}
-                        // onBlur={onChangeValidate}
-                    />
+                        <TextField
+                            required
+                            error={errorList.includes("fName")}
+                            disabled={true}
+                            id="fName"
+                            name="firstName"
+                            label="First name"
+                            value={appContext.user?.fName || " "}
+                            fullWidth
+                            variant="standard"
+                        />
 
-                </Grid>
+                    </Grid>
                 </Tooltip>
                 <Tooltip title={'You must update your profile to edit this field'}>
 
-                <Grid item xs={12} sm={5}>
-                    <TextField
-                         required
-                        disabled={true}
-                        id="lName"
-                        value={appContext.user?.lName || " "}
-                        // error = {errorList.includes("lName")}
-                        name="lastName"
-                        label="Last name"
-                        fullWidth
-                        // autoComplete="family-name"
-                        variant="standard"
-                        // onChange={(e) => {
-                        //     updateState(e)
-                        //     onChangeValidate(e);
-                        // }}
-                        //  onBlur={onChangeValidate}
-
-                    />
-                </Grid>
+                    <Grid item xs={12} sm={5}>
+                        <TextField
+                            required
+                            disabled={true}
+                            id="lName"
+                            value={appContext.user?.lName || " "}
+                            name="lastName"
+                            label="Last name"
+                            fullWidth
+                            variant="standard"
+                        />
+                    </Grid>
                 </Tooltip>
-                    <Tooltip title={'You must update your profile to edit this field'}>
+                <Tooltip title={'You must update your profile to edit this field'}>
 
                     <Grid item xs={12} sm={2}>
-                    <TextField
-                        disabled={true}
-                        error = {errorList.includes("mI")}
-                        id="mI"
-                        name="mI"
-                        value={appContext.user?.mI || " "}
-                        label="MI"
-                        fullWidth
-                        // autoComplete="middle-initial"
-                        variant="standard"
-                        // onChange={updateState}
+                        <TextField
+                            disabled={true}
+                            error={errorList.includes("mI")}
+                            id="mI"
+                            name="mI"
+                            value={appContext.user?.mI || " "}
+                            label="MI"
+                            fullWidth
+                            variant="standard"
+                        />
+                    </Grid>
+                </Tooltip>
+                <Tooltip title={'You must update your profile to edit this field'}>
 
+                    <Grid item xs={12} sm={5}>
+                        <TextField
+                            disabled={true}
+                            required
+                            error={errorList.includes("dodId")}
+                            id="dodId"
+                            name="dodId"
+                            value={appContext.user?.dodId || " "}
+                            label="Dod ID"
+                            type="number"
+                            fullWidth
+                            variant="standard"
+                        />
 
-                    />
-                </Grid>
-                    </Tooltip>
-                        <Tooltip title={'You must update your profile to edit this field'}>
-
-                        <Grid item xs={12} sm={5}>
-                    <TextField
-                        disabled={true}
-                        required
-                        error = {errorList.includes("dodId")}
-                        id="dodId"
-                        name="dodId"
-                        value={appContext.user?.dodId || " "}
-                        label="Dod ID"
-                        type="number"
-                        fullWidth
-                        // autoComplete="dodId"
-                        variant="standard"
-                        // onChange={updateState}
-                        // onBlur={onChangeValidate}
-
-
-                    />
-
-                </Grid>
-                        </Tooltip>
+                    </Grid>
+                </Tooltip>
 
 
                 <Tooltip title={'You must update your profile to edit this field'}>
-                <Grid item xs={12} sm={3} alignContent={"center"} alignItems={"center"}>
-                    {/*<Rank updateState={updateState} applicationInfo={applicationInfo} propsID={"rank"} value={applicationInfo.rank}/>*/}
-                    <TextField id={"rank"} variant="standard" value={appContext.user?.rank ?? "E1"} disabled={true}
-                               label={"Grade"} />
-                </Grid>
+                    <Grid item xs={12} sm={3} alignContent={"center"} alignItems={"center"}>
+                        {/*<Rank updateState={updateState} applicationInfo={applicationInfo} propsID={"rank"} value={applicationInfo.rank}/>*/}
+                        <TextField id={"rank"} variant="standard" value={appContext.user?.rank ?? "E1"} disabled={true}
+                                   label={"Grade"}/>
+                    </Grid>
                 </Tooltip>
-                    <Tooltip title={'You must update your profile to edit this field'}>
-                <Grid item xs={12} sm={4}>
-                    
-                    <TextField fullWidth={true} variant="standard" id={"dob"}
-                               value={(appContext.user?.dob) ? formatDate(appContext.user?.dob) : ''} disabled={true}
-                               label={"DOB"}/>
-                </Grid>
-                    </Tooltip>
+                <Tooltip title={'You must update your profile to edit this field'}>
+                    <Grid item xs={12} sm={4}>
+
+                        <TextField fullWidth={true} variant="standard" id={"dob"}
+                                   value={(appContext.user?.dob) ? formatDate(appContext.user?.dob) : ''}
+                                   disabled={true}
+                                   label={"DOB"}/>
+                    </Grid>
+                </Tooltip>
 
 
                 <Grid item xs={12} sm={6}>
-
-
-
 
 
                     <LastACFT updateState={updateState} applicationInfo={applicationInfo}
@@ -192,7 +144,6 @@ export default function PersonalInfoForm({
                         }
                         }
                     />
-
                 </Grid>
 
                 <Grid item xs={12} sm={6}>
@@ -237,12 +188,7 @@ export default function PersonalInfoForm({
                         onBlur={onChangeValidate}
                     />
                 </Grid>
-
             </Grid>
-
-
-
-
         </React.Fragment>
     );
 }
